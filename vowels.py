1ef3027b0a1a779f5b1533219bb0517017e85c96
BanashreeDas <banashreed2@gmail.com>
2025-03-09 22:12:38 +0530

    Create vowels.py

---
+++ vowels.py
@@ -0,0 +1,24 @@
+class FrequencyOfNumbers:
+    def VowelCount(str):
+        dict ={
+            "a":0,
+            "e":0,
+            "i":0,
+            "o":0,
+            "u":0
+        }
+
+        vowel="aeiou"
+
+        for c in str:
+            if c in vowel:
+                dict[c]+=1
+        
+        for c in vowel:
+            if dict[c]>0:
+                print(c ," : ",dict[c])
+
+
+str=input("Enter string : ").lower()
+ob=FrequencyOfNumbers
+ob.VowelCount(str)
